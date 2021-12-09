9510e3c7c46ba17b7e3e5ff169559f324fe604ae
Tommy Bidne <tbidne@protonmail.com>
2021-12-09 14:51:28 +1300

    Add PVP invariants to PackageVersion
    Enforce length > 2 and non-negative version numbers via smart
    constructor pattern.

--- src/Data/Version/Package.hs
+++ src/Data/Version/Package.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -21,14 +22,17 @@
 -- @since 0.1.0.0
 module Data.Version.Package
   ( -- * Type
-    PackageVersion (..),
+    PackageVersion (MkPackageVersion),
 
     -- ** Creation
+    mkPackageVersion,
+    unsafePackageVersion,
     fromVersion,
     fromString,
     fromText,
 
     -- ** Elimination
+    unPackageVersion,
     toVersion,
     toString,
     toText,
@@ -49,6 +53,7 @@ where
 import Control.Applicative qualified as A
 import Control.Exception.Safe (SomeException)
 import Control.Exception.Safe qualified as SafeEx
+import Control.Monad ((>=>))
 import Data.Attoparsec.Combinator qualified as AP
 import Data.Attoparsec.Text (Parser)
 import Data.Attoparsec.Text qualified as AP
@@ -77,39 +82,41 @@ import Text.Read qualified as TR
 -- numbers. It is similar to "Data.Version"'s 'Version' except:
 --
 -- 1. 'PackageVersion' has no 'versionTags'.
--- 2. Trailing zeroes are ignored in 'Eq', 'Ord', 'Semigroup', and 'Monoid'.
+-- 2. We enforce PVP's "tags must be at least A.B.C" invariant via the
+--    smart-constructor pattern.
+-- 3. Trailing zeroes are ignored in 'Eq', 'Ord', 'Semigroup', and 'Monoid'.
 --
 -- That is, we declare an equivalence class up to trailing zeroes.
 -- In particular, the 'Monoid' identity is
 --
 -- @
--- [0] = { [], [0], [0,0], ... }
+-- [0] = { [0,0,0], [0,0,0,0], ... }
 -- @
 --
 -- and its 'Semigroup' instance takes the greatest version (based on 'Ord').
 --
 -- Note: Because we export the underlying list in various ways,
--- (e.g. 'MkPackageVersion', 'show'), 'Eq'\'s extensionality can be broken:
+-- (e.g. 'show'), 'Eq'\'s extensionality can be broken:
 --
 -- @
 -- x == y ==> f x == f y
 -- @
 --
 -- ==== __Examples__
--- >>> MkPackageVersion [] == MkPackageVersion [0,0,0]
+-- >>> unsafePackageVersion [0,0,0,0] == unsafePackageVersion [0,0,0]
 -- True
 --
--- >>> MkPackageVersion [4] > MkPackageVersion [1,2]
+-- >>> unsafePackageVersion [4,0,0] > unsafePackageVersion [1,2,0,0]
 -- True
 --
--- >>> MkPackageVersion [5,6] <> MkPackageVersion [9,0]
--- MkPackageVersion {unPackageVersion = [9,0]}
+-- >>> unsafePackageVersion [5,6,0] <> unsafePackageVersion [9,0,0]
+-- UnsafePackageVersion {unPackageVersion = [9,0,0]}
 --
--- >>> MkPackageVersion [9] <> MkPackageVersion [9,0]
--- MkPackageVersion {unPackageVersion = [9]}
+-- >>> unsafePackageVersion [9,0,0] <> unsafePackageVersion [9,0,0,0]
+-- UnsafePackageVersion {unPackageVersion = [9,0,0]}
 --
 -- @since 0.1.0.0
-newtype PackageVersion = MkPackageVersion
+newtype PackageVersion = UnsafePackageVersion
   { -- | @since 0.1.0.0
     unPackageVersion :: [Int]
   }
@@ -127,13 +134,19 @@ newtype PackageVersion = MkPackageVersion
     )
 
 -- | @since 0.1.0.0
+pattern MkPackageVersion :: [Int] -> PackageVersion
+pattern MkPackageVersion v <- UnsafePackageVersion v
+
+{-# COMPLETE MkPackageVersion #-}
+
+-- | @since 0.1.0.0
 instance Eq PackageVersion where
-  MkPackageVersion v1 == MkPackageVersion v2 =
+  UnsafePackageVersion v1 == UnsafePackageVersion v2 =
     dropTrailingZeroes v1 == dropTrailingZeroes v2
 
 -- | @since 0.1.0.0
 instance Ord PackageVersion where
-  MkPackageVersion v1 `compare` MkPackageVersion v2 =
+  UnsafePackageVersion v1 `compare` UnsafePackageVersion v2 =
     dropTrailingZeroes v1 `compare` dropTrailingZeroes v2
 
 -- | @since 0.1.0.0
@@ -145,11 +158,11 @@ instance Semigroup PackageVersion where
 
 -- | @since 0.1.0.0
 instance Monoid PackageVersion where
-  mempty = MkPackageVersion []
+  mempty = UnsafePackageVersion [0, 0, 0]
 
 -- | @since 0.1.0.0
 instance Lift PackageVersion where
-  liftTyped (MkPackageVersion v) = [||MkPackageVersion v||]
+  liftTyped (UnsafePackageVersion v) = [||UnsafePackageVersion v||]
 
 dropTrailingZeroes :: (Eq a, Num a) => [a] -> [a]
 dropTrailingZeroes xs = take (lastNonZero xs) xs
@@ -159,25 +172,82 @@ dropTrailingZeroes xs = take (lastNonZero xs) xs
       | x /= 0 = (idx + 1, idx + 1)
       | otherwise = (idx + 1, acc)
 
--- | Creates a 'PackageVersion' from 'Version'. Note that because
--- 'PackageVersion' does not have a 'versionTags', 'fromVersion'
--- is not injective, i.e., @'toVersion' . 'fromVersion'@ is /not/
--- an isomorphism.
+-- | Smart constructor for 'PackageVersion'. The length of the list must be
+-- > 2 to match PVP's minimal A.B.C. Furthermore, all digits must be non-negative.
+--
+-- ==== __Examples__
+--
+-- >>> mkPackageVersion [1,2,3]
+-- Right (UnsafePackageVersion {unPackageVersion = [1,2,3]})
+--
+-- >>> mkPackageVersion [2,87,7,1]
+-- Right (UnsafePackageVersion {unPackageVersion = [2,87,7,1]})
+--
+-- >>> mkPackageVersion [1,2,-3,-4,5]
+-- Left "PackageVersion cannot contain any negative digits: -3"
+--
+-- >>> mkPackageVersion [3,2]
+-- Left "PackageVersion must have length > 2 to meet the PVP minimum A.B.C: [3,2]"
+--
+-- >>> mkPackageVersion []
+-- Left "PackageVersion must have length > 2 to meet the PVP minimum A.B.C: []"
+--
+-- @since 0.1.0.0
+mkPackageVersion :: [Int] -> Either String PackageVersion
+mkPackageVersion v@(_ : _ : _ : _) = case filter (< 0) v of
+  [] -> Right $ UnsafePackageVersion v
+  (neg : _) -> Left $ "PackageVersion cannot contain any negative digits: " <> show neg
+mkPackageVersion short =
+  Left $
+    "PackageVersion must have length > 2 to meet the PVP minimum A.B.C: "
+      <> show short
+
+-- | Unsafe version of 'mkPackageVersion', intended to be used with known
+-- constants.
+--
+-- __WARNING: This function is not total. Exercise restraint!__
+--
+-- ==== __Examples__
+-- >>> unsafePackageVersion [1,2,3]
+-- UnsafePackageVersion {unPackageVersion = [1,2,3]}
+--
+-- >>> unsafePackageVersion [1,2,-3]
+-- UnsafePackageVersion {unPackageVersion = *** Exception: PackageVersion cannot contain any negative digits: -3
+-- CallStack (from HasCallStack):
+--   error, called at src/Data/Version/Package.hs:226:31 in main:Data.Version.Package
+--
+-- >>> unsafePackageVersion [1]
+-- UnsafePackageVersion {unPackageVersion = *** Exception: PackageVersion must have length > 2 to meet the PVP minimum A.B.C: [1]
+-- CallStack (from HasCallStack):
+--   error, called at src/Data/Version/Package.hs:226:31 in main:Data.Version.Package
+--
+-- @since 0.1.0.0
+unsafePackageVersion :: [Int] -> PackageVersion
+unsafePackageVersion = either error id . mkPackageVersion
+
+-- | Creates a 'PackageVersion' from 'Version'.
+--
+-- Note: Because 'PackageVersion' does not have a 'versionTags', 'fromVersion'
+-- is not injective even on "well-formed" 'Version's (i.e. non-negative and length > 2).
+-- That is, @'toVersion' . 'fromVersion'@ is /not/ an isomorphism.
 --
 -- ==== __Examples__
 -- >>> fromVersion (Version [2,13,0] ["alpha"])
--- MkPackageVersion {unPackageVersion = [2,13,0]}
+-- Right (UnsafePackageVersion {unPackageVersion = [2,13,0]})
+--
+-- >>> fromVersion (Version [] [])
+-- Left "PackageVersion must have length > 2 to meet the PVP minimum A.B.C: []"
 --
 -- @since 0.1.0.0
-fromVersion :: Version -> PackageVersion
-fromVersion (Version v _) = MkPackageVersion v
+fromVersion :: Version -> Either String PackageVersion
+fromVersion = mkPackageVersion . versionBranch
 
 -- | Attempts to read a 'String' into a 'PackageVersion'. Leading and/or
 -- trailing dots will result in an error, as will the empty string.
 --
 -- ==== __Examples__
 -- >>> fromString "1.4.27.3"
--- Right (MkPackageVersion {unPackageVersion = [1,4,27,3]})
+-- Right (UnsafePackageVersion {unPackageVersion = [1,4,27,3]})
 --
 -- >>> fromString ""
 -- Left "Prelude.read: no parse"
@@ -191,6 +261,12 @@ fromVersion (Version v _) = MkPackageVersion v
 -- >>> fromString "1.2."
 -- Left "Prelude.read: no parse"
 --
+-- >>> fromString "1.3"
+-- Left "PackageVersion must have length > 2 to meet the PVP minimum A.B.C: [1,3]"
+--
+-- >>> fromString "-3.1.2"
+-- Left "PackageVersion cannot contain any negative digits: -3"
+--
 -- @since 0.1.0.0
 fromString :: String -> Either String PackageVersion
 fromString = fromText . T.pack
@@ -200,7 +276,7 @@ fromString = fromText . T.pack
 --
 -- ==== __Examples__
 -- >>> fromText "1.4.27.3"
--- Right (MkPackageVersion {unPackageVersion = [1,4,27,3]})
+-- Right (UnsafePackageVersion {unPackageVersion = [1,4,27,3]})
 --
 -- >>> fromText ""
 -- Left "Prelude.read: no parse"
@@ -214,26 +290,33 @@ fromString = fromText . T.pack
 -- >>> fromText "1.2."
 -- Left "Prelude.read: no parse"
 --
+-- >>> fromText "1.3"
+-- Left "PackageVersion must have length > 2 to meet the PVP minimum A.B.C: [1,3]"
+--
+-- >>> fromText "-3.1.2"
+-- Left "PackageVersion cannot contain any negative digits: -3"
+--
 -- @since 0.1.0.0
 fromText :: Text -> Either String PackageVersion
-fromText txt = MkPackageVersion <$> traverse (TR.readEither . T.unpack) sections
+fromText = readInts . splitDots >=> mkPackageVersion
   where
-    sections = T.split (== '.') txt
+    splitDots = T.split (== '.')
+    readInts = traverse (TR.readEither . T.unpack)
 
 -- | Creates a 'Version' with empty 'versionTags' from 'PackageVersion'.
 --
 -- ==== __Examples__
--- >>> toVersion (MkPackageVersion [3,2,0])
+-- >>> toVersion (UnsafePackageVersion [3,2,0])
 -- Version {versionBranch = [3,2,0], versionTags = []}
 --
 -- @since 0.1.0.0
 toVersion :: PackageVersion -> Version
-toVersion (MkPackageVersion v) = Version v []
+toVersion (UnsafePackageVersion v) = Version v []
 
 -- | Displays 'PackageVersion' in 'String' format.
 --
 -- ==== __Examples__
--- >>> toString (MkPackageVersion [2,7,10,0])
+-- >>> toString (UnsafePackageVersion [2,7,10,0])
 -- "2.7.10.0"
 --
 -- @since 0.1.0.0
@@ -243,7 +326,7 @@ toString = L.intercalate "." . fmap show . unPackageVersion
 -- | Displays 'PackageVersion' in 'Text' format.
 --
 -- ==== __Examples__
--- >>> toText (MkPackageVersion [2,7,10,0])
+-- >>> toText (UnsafePackageVersion [2,7,10,0])
 -- "2.7.10.0"
 --
 -- @since 0.1.0.0
@@ -255,7 +338,7 @@ toText = T.intercalate "." . fmap (T.pack . show) . unPackageVersion
 --
 -- ==== __Examples__
 -- >>> $$(packageVersionTH "package-version.cabal")
--- MkPackageVersion {unPackageVersion = [0,1,0,0]}
+-- UnsafePackageVersion {unPackageVersion = [0,1,0,0]}
 --
 -- @since 0.1.0.0
 #if MIN_VERSION_template_haskell(2, 17, 0)
@@ -319,7 +402,7 @@ packageVersionTextTH fp = TH.runIO (packageVersionTextIO fp) >>= liftTyped
 --
 -- ==== __Examples__
 -- >>> $$(packageVersionEitherTH "package-version.cabal")
--- Right (MkPackageVersion {unPackageVersion = [0,1,0,0]})
+-- Right (UnsafePackageVersion {unPackageVersion = [0,1,0,0]})
 --
 -- >>> $$(packageVersionEitherTH "not-found.cabal")
 -- Left "not-found.cabal: openFile: does not exist (No such file or directory)"
@@ -379,7 +462,7 @@ packageVersionTextIO fp = do
 --
 -- ==== __Examples__
 -- >>> packageVersionEitherIO "package-version.cabal"
--- Right (MkPackageVersion {unPackageVersion = [0,1,0,0]})
+-- Right (UnsafePackageVersion {unPackageVersion = [0,1,0,0]})
 --
 -- >>> packageVersionEitherIO "not-found.cabal"
 -- Left "not-found.cabal: openFile: does not exist (No such file or directory)"
@@ -403,17 +486,12 @@ parser =
   AP.string "version:"
     *> AP.skipSpace
     *> parseVers
-    <&> second MkPackageVersion
+    <&> (>>= mkPackageVersion)
   where
-    parseVers = traverse (readEither . T.unpack) <$> AP.many1 parseDigits
+    parseVers = traverse (TR.readEither . T.unpack) <$> AP.many1 parseDigits
     parseDigits =
       AP.takeWhile1 C.isDigit
         <* A.many (AP.string ".")
-
-readEither :: Read a => String -> Either String a
-readEither str = maybe (Left err) Right (TR.readMaybe str)
-  where
-    err = "Read error: " <> str
 
 hGetContents' :: IO.Handle -> IO String
 hGetContents' h = IO.hGetContents h >>= \s -> length s `seq` pure s

b712b8776e52d21772134acefb62c1e3651feddb
Tommy Bidne <tbidne@protonmail.com>
2022-01-28 23:02:22 +1300

    Add tests to 9.2.1 since HH has released new version

--- nix/ghc_base.nix
+++ nix/ghc_base.nix
@@ -1,7 +1,7 @@
 { compilerVersion }:
 
 let
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5b091d4fbe3b7b7493c3b46fe0842e4b30ea24b3.tar.gz") { };
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/945ec499041db73043f745fad3b2a3a01e826081.tar.gz") { };
   compiler = pkgs.haskell.packages."${compilerVersion}";
 in
 pkgs.mkShell {

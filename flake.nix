8554ea7cba85af214ddb8521cffebe66405472b5
Tommy Bidne <tbidne@protonmail.com>
2025-07-17 11:43:49 +1200

    Update nix deps

--- flake.nix
+++ flake.nix
@@ -4,22 +4,34 @@
   inputs.nix-hs-utils.url = "github:tbidne/nix-hs-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs =
-    inputs@{ flake-parts
-    , nix-hs-utils
-    , nixpkgs
-    , self
+    inputs@{
+      flake-parts,
+      nix-hs-utils,
+      nixpkgs,
+      self,
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      perSystem = { pkgs, ... }:
+      perSystem =
+        { pkgs, ... }:
         let
-          ghc-version = "ghc964";
+          ghc-version = "ghc9102";
           compiler = pkgs.haskell.packages."${ghc-version}";
-          mkPkg = returnShellEnv:
+          mkPkg =
+            returnShellEnv:
             nix-hs-utils.mkHaskellPkg {
               inherit compiler pkgs returnShellEnv;
               name = "package-version";
               root = ./.;
+
+              # TODO: Once hlint is back to working with our GHC we can
+              # use nix-hs-utils.mkDevTools ++ otherDeps.
+              devTools = [
+                (hlib.dontCheck compiler.cabal-fmt)
+                (hlib.dontCheck compiler.haskell-language-server)
+                pkgs.nixfmt-rfc-style
+              ];
             };
+          hlib = pkgs.haskell.lib;
           compilerPkgs = { inherit compiler pkgs; };
         in
         {
@@ -28,8 +40,8 @@
 
           apps = {
             format = nix-hs-utils.format compilerPkgs;
-            lint = nix-hs-utils.lint compilerPkgs;
-            lintRefactor = nix-hs-utils.lintRefactor compilerPkgs;
+            #lint = nix-hs-utils.lint compilerPkgs;
+            #lintRefactor = nix-hs-utils.lintRefactor compilerPkgs;
           };
         };
       systems = [
